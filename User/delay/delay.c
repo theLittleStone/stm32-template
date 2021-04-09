6bfa0cfba3e72b74f6de7e5dd36bb7dfe5c8d129
theLittleStone <thelittlestone@foxmail.com>
2021-04-09 15:03:54 +0800

    修正了delay函数参数为负时会挂的bug

--- User/delay/delay.c
+++ User/delay/delay.c
@@ -191,7 +191,8 @@ void delay_ms(u16 nms)
 //nus为要延时的us数.		    								   
 void delay_us(u32 nus)
 {		
-	u32 temp;	    	 
+	u32 temp;
+	if (temp <= 0) return;
 	SysTick->LOAD=nus*fac_us; 					//时间加载	  		 
 	SysTick->VAL=0x00;        					//清空计数器
 	SysTick->CTRL|=SysTick_CTRL_ENABLE_Msk ;	//开始倒数	  
@@ -210,7 +211,8 @@ void delay_us(u32 nus)
 //对72M条件下,nms<=1864 
 void delay_ms(u16 nms)
 {	 		  	  
-	u32 temp;		   
+	u32 temp;
+	if (temp <= 0) return;
 	SysTick->LOAD=(u32)nms*fac_ms;				//时间加载(SysTick->LOAD为24bit)
 	SysTick->VAL =0x00;							//清空计数器
 	SysTick->CTRL|=SysTick_CTRL_ENABLE_Msk ;	//开始倒数  
